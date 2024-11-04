7925c32a4d9d4f6e86595589d3d5fdb59fde6ac3
Marshverso <115442552+Marshverso@users.noreply.github.com>
2024-11-04 03:27:03 -0300

    Update Script Install.lua
    pfv, que seja a última correção

--- Script Install.lua
+++ Script Install.lua
@@ -1,3 +1,6 @@
+if versionW == nil then
+  versionW = 0
+end
 
 if not (buildTarget == 'windows') then
   close(false)
@@ -12,25 +15,20 @@ function downloadScript()
   saveFile('mods/scripts/Windows Funkin.lua', webScript:read("*a")..webScriptInstall:read("*a"), true)
   webScript:close()
   webScriptInstall:close()
-  restartSong(false)
 end
 
 -- Obter o código no github
-function onCreatePost()
-  if getDataFromSave('saiko', 'menu') then
-    -- obter a versão
-    local versionWindowsFunkin = io.popen("curl -s https://raw.githubusercontent.com/Marshverso/Windows-Funkin/main/Windows%20Funkin.lua")
-    local versionNumber = versionWindowsFunkin:read("*l")
-    versionWindowsFunkin:close()
-  
-    -- se a versão é desatualizada ou se você não tem ele, ele vai baixar
-    if versionW == nil then
-      versionW = 0
-    end
-    
-    if tonumber(versionW) < tonumber(versionNumber) then
-      downloadScript()
-    end
+if getDataFromSave('saiko', 'menu') then
+  -- obter a versão
+  local versionWindowsFunkin = io.popen("curl -s https://raw.githubusercontent.com/Marshverso/Windows-Funkin/main/Windows%20Funkin.lua")
+  local versionNumber = versionWindowsFunkin:read("*l")
+  versionNumber:match("(%d+%.%d+)")
+  versionWindowsFunkin:close()
+
+  -- se a versão é desatualizada ou se você não tem ele, ele vai baixar
+  if tonumber(versionW) < tonumber(versionNumber) then
+    downloadScript()
+    runTimer('rwf', 0.1)
   end
 end
 
@@ -40,5 +38,12 @@ function onUpdatePost()
     setDataFromSave('saiko', 'menu', not getDataFromSave('saiko', 'menu'))
     restartSong(false)
     close(false)
+  end
+end
+
+-- reiniciar caso baixe o script
+function onTimerCompleted(tag)
+  if tag == 'rwf' then
+    restartSong(false)
   end
 end
