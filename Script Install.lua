b2f8a303d24984e9e7e436fbfbd7a0e5a1ba93be
Marshverso <115442552+Marshverso@users.noreply.github.com>
2024-11-13 00:03:26 -0300

    arrumando agora

--- Script Install.lua
+++ Script Install.lua
@@ -2,23 +2,61 @@ if versionW == nil then
   versionW = 0
 end
 
-function onCreate()
+function onCreatePost()
   initSaveData('saiko', 'saiko')
 
+  if not getDataFromSave('saiko', 'menu') then
+    return Function_Stop
+  end
+
   -- Obter o código no github
-  if getDataFromSave('saiko', 'menu') then
-    -- obter a versão
-    local versionWindowsFunkin = io.popen("curl -s https://raw.githubusercontent.com/Marshverso/Windows-Funkin/main/Windows%20Funkin.lua")
-    local versionNumber = versionWindowsFunkin:read("*l")
-    local versionNumber = versionNumber:match("(%d+%.%d+)")
-    versionWindowsFunkin:close()
+  local versionWindowsFunkin = io.popen("curl -s https://raw.githubusercontent.com/Marshverso/Windows-Funkin/main/Windows%20Funkin.lua")
+  local versionNumber = versionWindowsFunkin:read("*l")
+  local versionNumber = versionNumber:match("(%d+%.%d+)")
+  versionWindowsFunkin:close()
 
     -- se a versão é desatualizada ou se você não tem ele, ele vai baixar
-    if tonumber(versionW) < tonumber(versionNumber) then
-      downloadScript()
-      runTimer('rwf', 0.1)
-    end
+  if tonumber(versionW) < tonumber(versionNumber) then
+    downloadScript()
+    runTimer('rwf', 0.1)
   end
+
+  --translator by Marshverso
+  verseTranslate('title', 'portuguese', 'WINDOWS FUNK')
+  verseTranslate('va', 'portuguese', 'Verificar arquivos')
+  verseTranslate('vh', 'portuguese', 'Verificar armazenamento') --professora de português é foda
+  verseTranslate('vr', 'portuguese', 'Verificar ram (REINICIAR PC)')
+  verseTranslate('rm', 'portuguese', "Remover marca d'água do windows (REINICIAR PC)")
+  verseTranslate('desfrag', 'portuguese', 'Otimizar armazenamento')
+  verseTranslate('cache', 'portuguese', 'Limpar cache')
+  verseTranslate('dn', 'portuguese', 'Limpar arquivos inúteis')
+  verseTranslate('odp', 'portuguese', 'Opções de desempenho')
+  verseTranslate('ld', 'portuguese', 'Limpar dns')
+  verseTranslate('sb', 'portuguese', 'Emulador do windows (REINICIAR PC)')
+  verseTranslate('an', 'portuguese', 'Ativar núcleos')
+
+  verseTranslate('storage', 'portuguese', 'DIGITE A LETRA DO SEU ARMAZENAMENTO')
+
+  --translator by Erislwlol and FacheFNF
+  verseTranslate('title', 'spanish', 'WINDOWS FUNK')
+  verseTranslate('va', 'spanish', 'Buscar archivos dañados')
+  verseTranslate('vh', 'spanish', 'Comproba si el disco está dañado') --professora de espanhol é foda
+  verseTranslate('vr', 'spanish', 'Comprobar ram (PC RESET)')
+  verseTranslate('rm', 'spanish', "Eliminar marca de agua de Windows (REINICIA EL PC)")
+  verseTranslate('desfrag', 'spanish', 'Optimice su disco')
+  verseTranslate('cache', 'spanish', 'Borrar la caché')
+  verseTranslate('dn', 'spanish', 'Eliminar archivos inútiles')
+  verseTranslate('odp', 'spanish', 'Opciones de rendimiento')
+  verseTranslate('ld', 'spanish', 'Borrar dns')
+  verseTranslate('sb', 'spanish', 'Emulador de windows (REINICIA EL PC)')
+  verseTranslate('an', 'spanish', 'Habilitar núcleos')
+
+  verseTranslate('storage', 'spanish', 'INGRESA LA LETRA DE TU ALMACENAMIENTO')
+  --Lo siento por cualquier cosa mal, lo hice para practicar mi español un poco
+
+  doTweenAlpha('camOtherAl', 'camOther', 1, 5, 'sineInOut')
+  selectionOp()
+  discord('WINDOWS FUNKIN', 'SELECT:'..getTextString(options.option[selection]))
 end
 
 -- baixar script
